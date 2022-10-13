9fa754baa97cd610ef77eafa5270f59df1e5f0a5
nascript <nascodefy@gmail.com>
2022-10-13 19:55:40 +0700

    banner image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,29 @@
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-import React from 'react'
+import { Movie } from '../typings'
+import { baseUrl } from '../constants/moview'
 
-const Banner = () => {
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+  console.log('moviee', movie)
   return (
     <div>
-      <div>
-        <Image src='' />
+      <div className='absolute top-0 left-0 h-[95vh] w-screen'>
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout='fill'
+        />
       </div>
     </div>
   )
