83b05775ec5a508c99a65c44bc8c00ad7ec7524f
nascript <nascodefy@gmail.com>
2022-10-15 23:19:14 +0700

    fix left arrow in row moview

--- components/Row.tsx
+++ components/Row.tsx
@@ -15,7 +15,7 @@ const Row = ({ title, movies }: Props) => {
         {title}
       </h2>
       <div className='group relative md:-ml-2'>
-        <HiChevronLeft />
+        <HiChevronLeft className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100' />
         <div className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2'>
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
