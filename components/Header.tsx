fec0855e4d278bd39c5a44b322f04443a112197d
nascript <nascodefy@gmail.com>
2022-10-15 23:40:21 +0700

    blind bg image for banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,27 +30,27 @@ const Header = () => {
           src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
           height='100'
           width='100'
-          className='cursor-pointer object-contain shadow-md'
+          className='cursor-pointer object-contain'
         />
 
         <ul className='hidden space-x-4 md:flex'>
-          <li className='headerLink text-shadow-lg'>Home</li>
-          <li className='headerLink text-shadow-lg'>Tv Shows</li>
-          <li className='headerLink text-shadow-lg'>Movies</li>
-          <li className='headerLink text-shadow-lg'>New & Popular</li>
-          <li className='headerLink text-shadow-lg'>My List</li>
+          <li className='headerLink text-shadow-md'>Home</li>
+          <li className='headerLink text-shadow-md'>Tv Shows</li>
+          <li className='headerLink text-shadow-md'>Movies</li>
+          <li className='headerLink text-shadow-md'>New & Popular</li>
+          <li className='headerLink text-shadow-md'>My List</li>
         </ul>
       </div>
 
       {/* RIGHT NAVBAR */}
       <div className='flex items-center space-x-4 text-sm font-ligth'>
-        <FiSearch className='hidden sm:inline h-6 w-6 shadow-md' />
-        <p className='hidden lg:inline shadow-md'>Kids</p>
-        <HiBell className='h-6 w-6 shadow-md' />
+        <FiSearch className='hidden sm:inline h-6 w-6' />
+        <p className='hidden lg:inline'>Kids</p>
+        <HiBell className='h-6 w-6' />
         <Link href='/account'>
           <img
             src='https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41'
-            className='cursor-pointer rounded shadow-md'
+            className='cursor-pointer rounded'
             alt='profile'
           />
         </Link>
